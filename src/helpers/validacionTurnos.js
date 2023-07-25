215ed7bb70517281abf8e99752634c0ff2d720af
Malena de Arriba <dearribamalena@gmail.com>
2023-07-24 23:33:11 -0300

    realice la validacion del detalle de la cita

---
+++ src/helpers/validacionTurnos.js
@@ -0,0 +1,16 @@
+import { check } from "express-validator";
+
+const validarTurno = [
+    check(detalleCita)
+        .notEmpty()
+        .withMessage("El detalle de la cita es obligatorio")
+        .isLength({ min: 2, max: 100 })
+        .withMessage(
+            "El detalle de la cita debe tener entre 2 y 100 caracteres como maximo"
+        ),
+    check(veterinario)
+    .notEmpty()
+    .withMessage("El veterinario es o")
+]
+
+export default validarTurno;

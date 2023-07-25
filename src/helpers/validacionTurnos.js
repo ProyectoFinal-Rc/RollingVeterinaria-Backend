a79328c7a9459b355766daee5b93082483a5f2d1
Malena de Arriba <dearribamalena@gmail.com>
2023-07-24 23:50:08 -0300

    valide la hora del turno y la forma de pago

--- src/helpers/validacionTurnos.js
+++ src/helpers/validacionTurnos.js
@@ -13,7 +13,7 @@ const validarTurno = [
     .notEmpty()
     .withMessage("El veterinario es obligatorio")
     .isIn(["karen diaz", "gerardo marruecos"])
-    .withMessage("Debe ingresar una categoria valida"),
+    .withMessage("Debe ingresar una categoria de veterinario valido"),
 
     check(mascota)
     .notEmpty()
@@ -27,8 +27,19 @@ const validarTurno = [
     .notEmpty()
     .withMessage("La fecha es obligatoria")
     .matches(/^(?:\d{4})\/(?:0[1-9]|1[0-2])\/(?:0[1-9]|[1-2][0-9]|3[0-1])$/)
-    .withMessage("La fecha debe tener el siguiente formato AAAA/MM/DD")
+    .withMessage("La fecha debe tener el siguiente formato AAAA/MM/DD"),
 
+    check(hora)
+    .notEmpty()
+    .withMessage("La hora es un campo obligatorio")
+    .isIn(["8:00", "9:00", "10:00", "11:00", "12:00", "17:00", "18:00", "19:00"])
+    .withMessage("Debe ingresar una hora valida"),
+
+    check(formaPago)
+    .notEmpty()
+    .withMessage("La forma de pago es un campo obligatorio")
+    .isIn(["efectivo", "tarjeta"])
+    .withMessage("Debe ingresar una forma de pago valida"),
 ]
 
 export default validarTurno;

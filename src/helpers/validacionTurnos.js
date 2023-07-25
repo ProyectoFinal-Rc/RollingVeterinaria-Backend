3369c3c48dd02284318cba6e9ec2b1f9a2df8fee
Malena de Arriba <dearribamalena@gmail.com>
2023-07-24 23:36:17 -0300

    realice la validacion del nombre de la mascota

--- src/helpers/validacionTurnos.js
+++ src/helpers/validacionTurnos.js
@@ -8,12 +8,21 @@ const validarTurno = [
         .withMessage(
             "El detalle de la cita debe tener entre 2 y 100 caracteres como maximo"
         ),
+        
     check(veterinario)
     .notEmpty()
     .withMessage("El veterinario es obligatorio")
     .isIn(["karen diaz", "gerardo marruecos"])
     .withMessage("Debe ingresar una categoria valida"),
-    
+
+    check(mascota)
+    .notEmpty()
+        .withMessage("El nombre de la mascota es obligatorio")
+        .isLength({ min: 2, max: 100 })
+        .withMessage(
+            "El nombre de la mascota debe tener entre 2 y 100 caracteres como maximo"
+        ),
+
 ]
 
 export default validarTurno;

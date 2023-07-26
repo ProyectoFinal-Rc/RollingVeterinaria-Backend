b9e0f614be449e429c6d8c5732e77514e330caf4
juanmllanos <juan_m@hotmail.com.ar>
2023-07-25 21:01:17 -0300

    Se agrego el controlador para crear un turno

---
+++ src/controllers/turno.controllers.js
@@ -0,0 +1,17 @@
+import Turno from "../models/turno";
+
+export const crearTurno = async (req, res) => {
+	try {
+		const turnoNuevo = new Turno(req.body);
+		await turnoNuevo.save();
+		res.status(201).json({
+			mensaje: 'El turno se creo con exito.'
+		})
+	} catch (error) {
+		console.log(error);
+		res.status(404).json({
+			mensaje: 'Error al crear el turno.'
+		})
+		
+	}
+}

2f84cca43af33687945770b1f2c1eea5995259b8
juanmllanos <juan_m@hotmail.com.ar>
2023-07-25 23:39:30 -0300

    Se creo logica de editar turno

--- src/controllers/turno.controllers.js
+++ src/controllers/turno.controllers.js
@@ -27,6 +27,20 @@ export const obtenerTurnos = async (req, res) => {
 	}
 }
 
+export const editarTurno = async (req, res) => {
+	try {
+		await Turno.findByIdAndUpdate(req.params.id, req.body);
+		res.status(200).json({
+			mensaje: 'El turno se modifico correctamente.'
+		})
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			mensaje: 'Error al intentar editar el turno.'
+		})
+	}
+}
+
 export const borrarTurno = async (req, res) => {
 	try {
 		await Turno.findByIdAndRemove(req.params.id)

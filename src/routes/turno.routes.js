24192147d8e74292d8dda6841bafe332bdea1fed
juanmllanos <juan_m@hotmail.com.ar>
2023-07-26 18:49:14 -0300

    Se elimino el archivo de rutas de turno

--- src/routes/turno.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { crearTurno } from '../controllers/turno.controllers'
-
-const router = Router()
-
-router.route('/').post(crearTurno)
-
-export default router

61dc209cde81b17c8415c1c972ff92353ac0a0dc
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2023-08-10 09:38:13 -0300

    arregladas respuestas para manejar error

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -21,19 +21,19 @@ export const loguearUsuario = async (req, res) => {
           process.env.SECRET_KEY
         );
         if (token) {
-          res.status(200).json({ mensaje: "contraseña ok", token,usuario });
+          res.status(200).json({ mensaje: "contraseña ok", token,usuario, error:false });
         } else {
-          res.status(500).json({ mensaje: "error al generer token" });
+          res.status(500).json({ mensaje: "error al generer token", error:true });
         }
       } else {
-        res.status(401).json({ mensaje: "contraseña incorrecta" });
+        res.status(401).json({ mensaje: "contraseña incorrecta", error:true });
       }
     } else {
-      res.status(404).json({ mensaje: "no se encontró usuario" });
+      res.status(404).json({ mensaje: "no se encontró usuario", error:true });
     }
   } catch (error) {
     res.status(500).json({
-      mensaje: "Error al consultar datos: " + error.message,
+      mensaje: "Error al consultar datos: " + error.message, error:true
     });
   }
 };
@@ -41,27 +41,27 @@ export const loguearUsuario = async (req, res) => {
 export const checkTokenAdmin = async (req, res) => {
   try {
     if (adminTokenStatus(req.headers["x-token"], process.env.SECRET_KEY)) {
-      res.status(200).json({ mensaje: "token valido" });
+      res.status(200).json({ mensaje: "token valido", error:false });
     } else {
       res
         .status(401)
-        .json({ mensaje: "token invalido / error al decodificar token" });
+        .json({ mensaje: "token invalido / error al decodificar token" , error:true});
     }
   } catch (err) {
-    res.status(500).json({ mensaje: "error interno / token invalido: " + err });
+    res.status(500).json({ mensaje: "error interno / token invalido: " + err , error:true});
   }
 };
 
 export const checkTokenUser = async (req, res) => {
   try {
     if (userTokenStatus(req.headers["x-token"], process.env.SECRET_KEY)) {
-      res.status(200).json({ mensaje: "token valido" });
+      res.status(200).json({ mensaje: "token valido", error:false });
     } else {
       res
         .status(401)
-        .json({ mensaje: "token invalido / error al decodificar token" });
+        .json({ mensaje: "token invalido / error al decodificar token", error:true });
     }
   } catch (err) {
-    res.status(500).json({ mensaje: "error interno / token invalido: " + err });
+    res.status(500).json({ mensaje: "error interno / token invalido: " + err ,error:true});
   }
 };
